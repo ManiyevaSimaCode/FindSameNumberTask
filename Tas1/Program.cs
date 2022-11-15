264faf71169419a50522f3ebaf4a2420c0eec44a
ManiyevaSimaCode <116644266+ManiyevaSimaCode@users.noreply.github.com>
2022-11-15 18:28:44 +0400

    Add files via upload

---
+++ Tas1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Random rand = new Random();
+int number = rand.Next(1, 25);
+Console.WriteLine("number:" + number);
+int count=Convert.ToInt32(Console.ReadLine());
+int cehd = count;
+int say = 0;
+
+while (count>0)
+{
+
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > number)
+    {
+        Console.WriteLine("Daxil etdiyiniz eded ededden yuxaridadir");
+    }
+    else if (num < number)
+    {
+        Console.WriteLine("Daxil etdiyiniz eded ededden ashagidadir");
+    }
+    else
+    {
+       	Console.WriteLine("Dogrudur");
+        Console.WriteLine($"Ededi {say+1} cehdde tapdiniz");
+        Console.WriteLine($"baliniz:{(100 - say * (100 / cehd))}");
+        break;
+    }
+    say++;
+    count--;
+    Console.WriteLine($"{count} shansiniz qaldi");
+
+}
+
+
