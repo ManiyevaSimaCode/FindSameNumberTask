c3c34b798c385ff81c5e2992e78f6d0c8a206be0
ManiyevaSimaCode <116644266+ManiyevaSimaCode@users.noreply.github.com>
2022-11-15 19:34:54 +0400

    Add files via upload

---
+++ Task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿string word=Console.ReadLine();
+string []value =word.Split(" ");
+
+string newWord="";
+for (int i = 0; i < value.Length; i++)
+{
+    for (int j = value[i].Length-1 ;j>=0; j--)
+    {
+        newWord += value[i][j];
+    }
+    newWord += " ";
+}
+
+Console.WriteLine(newWord);
